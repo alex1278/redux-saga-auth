e2d82eab39fcd65c099ad5275aaf782d75d6b7e9
Elliot Plant <elliotaplant@gmail.com>
2018-05-10 20:41:28 -0700

    Add signup form - but no change in redux initial state

--- src/index-reducer.ts
+++ src/index-reducer.ts
@@ -1,10 +1,20 @@
 // index-reducer.ts
 import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
+import { FormStateMap, reducer as form } from 'redux-form';
+import { ClientState } from './client/constants';
 import client from './client/reducer';
+import { SignupState } from './signup/constants';
+import signup from './signup/reducer';
+
+export interface RootState {
+  client: ClientState;
+  signup: SignupState;
+  forms: FormStateMap; // ?
+}
 
 const IndexReducer = combineReducers({
   client,
+  signup,
   form,
 });
 
--- src/signup/index.tsx
+++ src/signup/index.tsx
@@ -1,5 +1,30 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { InjectedFormProps, reduxForm } from 'redux-form';
+import { RootState } from '../index-reducer';
+import signupRequest from './actions';
 
-class Signup extends React.Component {}
+// Will this type be enough?
+// If trouble, see https://spin.atomicobject.com/2017/04/20/typesafe-container-components/
+class Signup extends React.Component<InjectedFormProps> {
+  render() {
+    return <div>Signup</div>;
+  }
+}
 
-export default Signup;
+// Grab only the piece of state we need
+const mapStateToProps = (state: RootState) => ({
+  signup: state.signup,
+});
+
+// Connect our component to redux and attach the `signup` piece
+// of state to our `props` in the component. Also attach the
+// `signupRequest` action to our `props` as well.
+const connected = connect(mapStateToProps, { signupRequest })(Signup);
+
+// Connect our connected component to Redux Form.  It will namespace
+// the form we use in this component as `signup`.
+const formed = reduxForm({ form: 'signup' })(connected);
+
+// Export our well formed component!
+export default formed;
