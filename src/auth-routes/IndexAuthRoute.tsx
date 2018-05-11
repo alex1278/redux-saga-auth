6b1cc1b16508e87ba85494a0dea27c77967de2fb
Elliot Plant <elliotaplant@gmail.com>
2018-05-11 00:28:42 -0700

    Jk not working at all

--- src/auth-routes/IndexAuthRoute.tsx
+++ src/auth-routes/IndexAuthRoute.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { RoutePropsWithComponent } from './constants';
 import PrivateRoute from './PrivateRoute';
 
-const isAuthed = () => true; // change to actual auth funciton
+const isAuthed = () => false; // change to actual auth funciton
 
 export const IndexAuthRoute = (props: RoutePropsWithComponent) => (
   <PrivateRoute authFn={isAuthed} component={props.component} />
