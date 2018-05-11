1e52cdfc84d184621d916c067567139b5a30320d
Elliot Plant <elliotaplant@gmail.com>
2018-05-10 17:14:06 -0700

    cp

--- src/index-reducer.ts
+++ src/index-reducer.ts
@@ -1,8 +1,10 @@
 // index-reducer.ts
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import client from './client/reducer';
 
 const IndexReducer = combineReducers({
+  client,
   form,
 });
 
